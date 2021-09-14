ab61f72a4a7d68f4e85a0c724fdac57ceecbb8eb
Tin Lai <oscar@tinyiu.com>
2021-09-14 15:06:59 +1000

    add an easy to use interface for gazebo with kinova jaco
    Signed-off-by: Tin Lai <oscar@tinyiu.com>

---
+++ nodes/interface/kinova_interface.py
@@ -0,0 +1,61 @@
+#! /usr/bin/env python3
+
+import time
+from typing import Union, List, Tuple
+
+import rospy
+
+# import moveit_commander
+from geometry_msgs.msg import PoseStamped
+from moveit_msgs.msg import RobotState
+from sensor_msgs.msg import JointState
+
+from std_msgs.msg import Header, Float64
+from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
+
+
+class KinovaInterface:
+    def __init__(self, arm_type: str="j2n6s300", n_dim: int=6, wait: bool=True):
+        super().__init__()
+        self.n_dim = n_dim
+        self.arm_type = arm_type
+        self.cached_msg = None
+
+        rospy.init_node("gazebo_kinova_interface", anonymous=False)
+
+        self.effort_joints_pub = [
+            rospy.Publisher(
+                f"/{self.arm_type}/joint_{i + 1}_effort_controller/command",
+                Float64,
+                queue_size=10,
+            )
+            for i in range(self.n_dim)
+        ]
+
+        self.subscriber = rospy.Subscriber(
+            f"/{self.arm_type}/joint_states", JointState, self.new_state_cb
+        )
+
+        if wait:
+            time.sleep(1)
+
+    def new_state_cb(self, msg):
+        self.cached_msg = msg
+
+    def send_joint_effort(self, tau: List[Union[float, int, None]]) -> None:
+        """
+        Given a list of numbers (or None), send them as the force to be applied on
+        each joint. If None is given, that joint will be skipped.
+        """
+        assert len(tau) == self.n_dim, tau
+        for force, pub in zip(tau, effort_joints_pub):
+            if force is not None:
+                pub.publish(Float64(force))
+
+    def get_current_state(self) -> Tuple[List[float], List[float]]:
+        """
+        Return the current q and qd of the arm.
+        IMPORTANT: Note that ros/kinova does not automatically wraps the values,
+            so the joint rotational angles will not be within [-pi, pi).
+        """
+        return self.cached_msg.position, self.cached_msg.velocity
